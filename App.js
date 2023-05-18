adf19f353ec29b24ba14f27673f994a85c6eec56
Bala Bhaskar <103302480+balabgcode@users.noreply.github.com>
2023-05-18 10:04:21 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.css';function App() {
+ return (
+   <div className="App">
+        Hello World! Starting a to-do List
+   </div>
+ );
+}export default App;
